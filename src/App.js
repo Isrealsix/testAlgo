c8290aaebfad373ddc790ff13732ae9bca35e32d
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2022-07-27 16:45:14 +0700

    completed TDD

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [buttonColor, setButtonColor] = useState("red");
+  const newButtonColor = buttonColor === "red" ? "blue" : "red";
   return (
     <div>
-      <button style={{ backgroundColor: "red" }}>change to blue</button>
+      <button
+        style={{ backgroundColor: buttonColor }}
+        onClick={() => setButtonColor(newButtonColor)}
+      >
+        change to {newButtonColor}
+      </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 test("button has correct initial color", () => {
@@ -9,6 +9,15 @@ test("button has correct initial color", () => {
 
   // expect background color to be red
   expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  // Using the fireEvent to make a click on the button
+  fireEvent.click(colorButton);
+
+  // After click, we expect the button background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+
+  // Also expect the textContent to change
+  expect(colorButton.textContent).toBe("change to red");
 });
 
 test("button has correct initial text", () => {});
