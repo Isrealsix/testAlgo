e4b9b611d2b4036b5ae604d784001cb6b6816c1a
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2022-07-31 05:52:05 +0700

    added comments

--- src/ts.js
+++ src/ts.js
@@ -1,6 +1,19 @@
 // Find 2 numbers, when added, equals the input that was provided
 const ages = [1, 4, 2, 10, 3, 5, 7, 6, 8, 9];
 // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+/**
+ * you have an array of elements [1, 4, 2, 10, 3, 5, 7, 6, 8, 9]
+ * you have a function that takes in the two parameters  a => array and b => totalSum.
+ * The function should find 2 consecutive numbers in the array
+ * that when added it is equal to the totalSum(b).
+ * findSum([1, 4, 2, 10, 3, 5, 7, 6, 8, 9], 10)
+ * @returns 1,9
+ * 2,8
+ * 3,7
+ * 4,6
+ */
+
 export const calcADD = (arr, count) => {
   let tempCount = 0;
   let idx = 0;
